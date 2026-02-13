45960535a2fc2780a8c7292f99180d5ff92016de
adamhidara58-tech <fhncis12@gmail.com>
2026-02-13 05:57:27 -0500

    Fix Safari compatibility: Add -webkit prefixes and replace clip-path with CSS borders

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -211,8 +211,11 @@ const Team = () => {
             
             {/* Pointer */}
             <div className="absolute -top-4 left-1/2 -translate-x-1/2 z-30 drop-shadow-2xl">
-              <div className="w-8 h-10 bg-gradient-to-b from-white to-gold clip-path-pointer" style={{ clipPath: 'polygon(50% 100%, 0 0, 100% 0)' }} />
-              <div className="w-2 h-2 bg-white rounded-full absolute top-1 left-1/2 -translate-x-1/2" />
+              <div 
+                className="w-0 h-0 border-l-[16px] border-l-transparent border-r-[16px] border-r-transparent border-t-[24px] border-t-gold relative"
+              >
+                <div className="absolute -top-[26px] left-1/2 -translate-x-1/2 w-2 h-2 bg-white rounded-full" />
+              </div>
             </div>
             
             {/* SVG Wheel */}
